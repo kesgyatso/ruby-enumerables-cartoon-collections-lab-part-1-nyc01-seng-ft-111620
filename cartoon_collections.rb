0b601d3a4bc188d31594b19ca7c62dccbca46ab4
Kelsang Gyatso <kelsanggyatso@gmail.com>
2020-11-11 05:40:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ def greet_characters(array)
   
   array.each do |element|
     p "Hello #{element}!"
-  
+  end
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
