8baa0ec784bf8d153d8f1273b42d9af60abe151f
Kelsang Gyatso <kelsanggyatso@gmail.com>
2020-11-11 05:41:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,6 +9,11 @@ def greet_characters(array)
 end
 
 def list_dwarves(array)
+  
+  array.each_with_index do |element, index|
+    
+    
+    
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
