d7441a1992c0d239c84643cec239b2f785beae88
Kelsang Gyatso <kelsanggyatso@gmail.com>
2020-11-11 05:41:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,6 +13,8 @@ def list_dwarves(array)
   array.each_with_index do |element, index|
     
     puts "#{index + 1}. #{element}"
+   
+  end 
     
   # Use `each_with_index` to enumerate over the provided array
   #
